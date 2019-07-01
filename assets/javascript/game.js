9dfaf9a21fa67256e8c817fcd03f111ea8690fd4
Quinlan Hampton <qnhampton93@gmail.com>
2019-06-30 17:39:42 -0700

    adding: commented html, css, and js files, basic html code/structure, and all images to be used in game (compressed)

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+//call elements from html and set up as variables
+
+//set up variables using in js
+
+//reset game score bored to zeros
+
+//randomize computer choices
+
+//document key up and dertimin which key is pressed
+
+//alerts you've made a guess of ""
+
+//if else condtions *rememder to increase scores by x and decrease guesses by y
+
+//if guessed correct: alter you're psychic, show kadabra, increase winScore
+
+//else if guessed incorrect: alter oh no..., show psyduck, decrease guessCounter
+
+//else if guessCounter is zero: alert you've given psyduck a headache, alert he uses confusion, alert you lose, show teamrocket 
+
+//else if winScore is 10: alert you've won- your kadabra evolved into an alakazam, show alakazam
+
+//print score to scoreboard 
+
+//reset button- reset score without the whole page
